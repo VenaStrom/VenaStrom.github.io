3582c7653cada206b65633b76d4467362cf7098d
Vena <strom.vena+gh@gmail.com>
2025-09-20 12:09:26 +0200

    update about me styling

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -2,23 +2,34 @@ import { info } from "../info.tsx";
 
 export default function AboutMe({ className = "" }: { className?: string }) {
   return (
-    <section className={className}>
+    <section className={`${className} gap-y-3`}>
       <h3>About Me</h3>
 
-      <p>{info.aboutMe.description}</p>
-
-      <ul>
-        {info.aboutMe.languages.map((language, i) => (
-          <li key={`language-${i}`}>{language}</li>
+      <p className="flex flex-col gap-3">
+        {info.aboutMe.description.map((item, i) => (
+          <span key={`description-item-${i}`}>
+            {item}
+          </span>
         ))}
-      </ul>
+      </p>
 
-      <h4 title="I struggle to find a better name">Things...</h4>
-      <ul>
-        {info.aboutMe.randomThings.map((thing, i) => (
-          <li key={`thing-${i}`}>{thing}</li>
-        ))}
-      </ul>
+      <div>
+        <h4>Language</h4>
+        <ul>
+          {info.aboutMe.languages.map((language, i) => (
+            <li key={`language-${i}`}>{language}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h4 title="I struggle to find a better name" className="underline decoration-dotted cursor-help">Things...</h4>
+        <ul className="list-disc list-inside">
+          {info.aboutMe.randomThings.map((thing, i) => (
+            <li key={`thing-${i}`}>{thing}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
