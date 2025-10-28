443187ce1d2f8f215b7306c24841a4896699b38d
Vena <strom.vena+gh@gmail.com>
2025-10-28 14:55:23 +0100

    change image sizes

--- scripts/webp-converter.ts
+++ scripts/webp-converter.ts
@@ -7,10 +7,17 @@ if (!webpPath) {
 import { execSync } from "child_process";
 import fs from "node:fs";
 import path from "node:path";
-
+import sharp from "sharp";
 
 const inputImages = fs.globSync("assets/screenshots/*.png");
 const outputDir = "public/screenshots/";
+
+const outputSizeOverride = {
+  "feed-page.png": { width: 400, height: 0 },
+  "home-page.png": { width: 400, height: 0 },
+  "playlist-editor-exporting.png": { width: 0, height: 600 },
+  "playlist-editor.png": { width: 0, height: 600 },
+}
 
 for (const inputImage of inputImages) {
   const outputImage = path.join(
@@ -18,7 +25,24 @@ for (const inputImage of inputImages) {
     path.basename(inputImage, path.extname(inputImage)) + ".webp",
   );
 
-  const command = `"${webpPath}" -q 80 "${inputImage}" -o "${outputImage}"`;
+  let outputWidth: number | undefined = undefined;
+  let outputHeight: number | undefined = undefined;
+  const override = outputSizeOverride[path.basename(inputImage)];
+  if (override) {
+    outputWidth = override.width;
+    outputHeight = override.height;
+  }
+  else {
+    const scale = 0.5;
+    const metadata = await sharp(inputImage).metadata();
+    if (!metadata.width || !metadata.height) {
+      throw new Error(`Could not get dimensions for image: ${inputImage}`);
+    }
+    outputWidth = Math.round(metadata.width * scale);
+    outputHeight = Math.round(metadata.height * scale);
+  }
+
+  const command = `"${webpPath}" "${inputImage}" -resize ${outputWidth} ${outputHeight} -o "${outputImage}"`;
   console.log(`Converting ${inputImage} to ${outputImage}...`);
   execSync(command);
   console.log(`Converted ${inputImage} to ${outputImage}`);
